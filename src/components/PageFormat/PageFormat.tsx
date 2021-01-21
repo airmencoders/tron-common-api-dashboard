35e01a436e5dbe68c92beb798cd3c1314020cfaf
Chad Kumabe <ckumabe@gmail.com>
2021-01-21 12:08:16 -1000

    [TRONAPI-127] Add page layout component.

--- src/components/PageFormat/PageFormat.tsx
+++ src/components/PageFormat/PageFormat.tsx
@@ -1,4 +1,7 @@
 import React, {ReactNode} from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+
+import './PageFormat.scss';
 
 export interface PageFormatProps {
   pageTitle: string;
@@ -8,13 +11,22 @@ export interface PageFormatProps {
 function PageFormat(props: any) {
   return (
       <div className="page-format">
-        <div className="page-format__nav-menu">
-          Nav Menu
+        <div className="page-format__nav-menu default-panel-padding">
+          <h3 style={{color: 'white'}}>Nav Menu</h3>
         </div>
         <div className="page-format__page-body-container">
-
+          <div className="page-format__top-nav  default-panel-padding">
+            <h4>Top Nav</h4>
+          </div>
+          <div className="page-format__page-body">
+            <div className="page-body__title-section default-panel-padding">
+              <PageTitle title={props.pageTitle} />
+            </div>
+            <div className="page-body__content default-panel-padding">
+              {props.children}
+            </div>
+          </div>
         </div>
-        {props.children}
       </div>
   );
 }
