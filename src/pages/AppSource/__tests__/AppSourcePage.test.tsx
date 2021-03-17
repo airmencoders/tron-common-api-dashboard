840ef116266f5a45849ed70b2e3965108d8217a9
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-03-17 01:10:49 -1000

    Implement view only for App Source Page

---
+++ src/pages/AppSource/__tests__/AppSourcePage.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { createState, State, StateMethodsDestroy } from '@hookstate/core';
+import { AppSourceControllerApi, AppSourceControllerApiInterface, AppSourceDto } from '../../../openapi';
+import { AppSourcePage } from '../AppSourcePage';
+import AppSourceService from '../../../state/app-source/app-source-service';
+import { useAppSourceState } from '../../../state/app-source/app-source-state';
+
+jest.mock('../../../state/app-source/app-source-state');
+
+describe('Test App Source Page', () => {
+  let appSourceState: State<AppSourceDto[]> & StateMethodsDestroy;
+  let appSourceApi: AppSourceControllerApiInterface;
+
+  beforeEach(() => {
+    appSourceState = createState<AppSourceDto[]>(new Array<AppSourceDto>());
+    appSourceApi = new AppSourceControllerApi();
+  });
+
+  it('Test Loading Page', async () => {
+    function mockAppSourceState() {
+      (useAppSourceState as jest.Mock).mockReturnValue(new AppSourceService(appSourceState, appSourceApi));
+
+      jest.spyOn(useAppSourceState(), 'isPromised', 'get').mockReturnValue(true);
+    }
+
+    mockAppSourceState();
+
+    const page = render(
+      <MemoryRouter>
+        <AppSourcePage />
+      </MemoryRouter>
+    );
+
+    expect(page.getByText('Loading...')).toBeDefined();
+  });
+})
