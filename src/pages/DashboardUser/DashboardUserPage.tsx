35f90c80a8b213766f42c716a15b18171cf08473
Dylan Nakamoto <dylannakamoto@Dylans-MacBook-Pro.local>
2021-03-30 22:18:20 -1000

    Move privilege dependency out from component to service

--- src/pages/DashboardUser/DashboardUserPage.tsx
+++ src/pages/DashboardUser/DashboardUserPage.tsx
@@ -1,17 +1,60 @@
-import { useEffect } from "react";
-import { usePrivilegeState } from "../../state/privilege/privilege-state";
-import { DashboardUserContent } from "./DashboardUserContent";
+import React from 'react';
+import { DataCrudFormPage } from '../../components/DataCrudFormPage/DataCrudFormPage';
+import GridColumn from '../../components/Grid/GridColumn';
+import PrivilegeCellRenderer from '../../components/PrivilegeCellRenderer/PrivilegeCellRenderer';
+import { DashboardUserFlat } from '../../state/dashboard-user/dashboard-user-flat';
+import { useDashboardUserState } from '../../state/dashboard-user/dashboard-user-state';
+import DashboardUserDelete from './DashboardUserDelete';
+import DashboardUserForm from './DashboardUserForm';
+
+const columns: GridColumn[] = [
+  new GridColumn({
+    field: 'id',
+    sortable: true,
+    filter: true,
+    headerName: 'UUID'
+  }),
+  new GridColumn({
+    field: 'email',
+    sortable: true,
+    filter: true,
+    headerName: 'Email'
+  }),
+  new GridColumn({
+    field: 'hasDashboardAdmin',
+    sortable: true,
+    filter: true,
+    headerName: 'Dashboard Admin',
+    headerClass: 'header-center',
+    cellRenderer: PrivilegeCellRenderer
+  }),
+  new GridColumn({
+    field: 'hasDashboardUser',
+    sortable: true,
+    filter: true,
+    headerName: 'Dashboard User',
+    headerClass: 'header-center',
+    cellRenderer: PrivilegeCellRenderer
+  }),
+];
 
 function DashboardUserPage() {
-  const privilegeState = usePrivilegeState();
-
-  useEffect(() => {
-    privilegeState.fetchAndStorePrivileges();
-  }, []);
-
   return (
-    <DashboardUserContent />
-  );
+    <DataCrudFormPage<DashboardUserFlat, DashboardUserFlat>
+      columns={columns}
+      dataTypeName='Dashboard User'
+      pageTitle='Dashboard Users'
+      createForm={DashboardUserForm}
+      updateForm={DashboardUserForm}
+      useDataState={useDashboardUserState}
+      allowEdit={true}
+      allowDelete
+      allowAdd
+      deleteComponent={DashboardUserDelete}
+      autoResizeColumns
+      autoResizeColummnsMinWidth={1200}
+    />
+  )
 }
 
 export default DashboardUserPage;
