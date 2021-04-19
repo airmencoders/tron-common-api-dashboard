8609f4a1c1392f8231e4a7fd59983ccfb6866601
William De Witt <>
2021-04-19 10:12:00 -0400

    Tests for metrics services

--- src/icons/MetricIcon.tsx
+++ src/icons/MetricIcon.tsx
@@ -2,7 +2,7 @@ import { IconProps } from './IconProps';
 
 function MetricIcon(props: IconProps) {
   return (
-      <i className="metric-icon bi bi-bar-chart-line"
+      <i className={`metric-icon bi bi-bar-chart-line ${props.className}`}
          style={{fontSize: `${props.size}rem`}}
          title={ props.iconTitle != null ? props.iconTitle : 'metric'}
       ></i>
