fcb000a672b01d9e36eb787ad70bd8f1a91e28b8
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-05-03 09:13:58 -1000

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,9 +5,9 @@ import { Button as UswdsButton } from '@trussworks/react-uswds/lib/index'
 import './Button.scss';
 
 function Button(props: ButtonProps) {
-  const { disableMobileFullWidth } = props;
+  const { disableMobileFullWidth, ...rest } = props;
   return (
-    <UswdsButton {...props} className={`${props.className ?? ''}${disableMobileFullWidth ? ' usa-button--disable-mobile-full-width' : ''}`} />
+    <UswdsButton {...rest} className={`${props.className ?? ''}${disableMobileFullWidth ? ' usa-button--disable-mobile-full-width' : ''}`} />
   );
 }
 
