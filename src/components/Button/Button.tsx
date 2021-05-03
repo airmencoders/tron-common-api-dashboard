d94821bb96608ba75c99cd916f75848251d76741
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-05-03 09:08:09 -1000

    Remove disableMobileFullWidth from getting passed to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,8 +5,9 @@ import { Button as UswdsButton } from '@trussworks/react-uswds/lib/index'
 import './Button.scss';
 
 function Button(props: ButtonProps) {
+  const { disableMobileFullWidth } = props;
   return (
-    <UswdsButton {...props} className={`${props.className ?? ''}${props.disableMobileFullWidth ? ' usa-button--disable-mobile-full-width' : ''}`} />
+    <UswdsButton {...props} className={`${props.className ?? ''}${disableMobileFullWidth ? ' usa-button--disable-mobile-full-width' : ''}`} />
   );
 }
 
