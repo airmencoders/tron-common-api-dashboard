565ee305105841d9e75503add77242328708b53a
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-06-24 01:18:05 -1000

    Reduce cognitive complexity for creating json patch in organization service

--- src/state/organization/organization-service.ts
+++ src/state/organization/organization-service.ts
@@ -424,72 +424,18 @@ export default class OrganizationService extends AbstractDataService<Organizatio
       return Promise.reject(new Error('Organization to update has undefined id.'));
     }
 
-    const jsonPatchOperations: Array<JsonPatchStringArrayValue | JsonPatchStringValue | JsonPatchObjectValue | JsonPatchObjectArrayValue> = this.createJsonPatch(original, toUpdate, toPatch);
-
     /**
-     * Check to find only removals that exist in the original. If it does not exist
-     * in the original, then there's no need to add it to the request for removal.
+     * Will contains all of the requests that are sent to patch
+     * the organization for update.
      */
-    let subOrgsToRemove: string[] = [];
-    if (original.subordinateOrganizations != null) {
-      subOrgsToRemove = getDataItemDuplicates(original.subordinateOrganizations, toPatch.subOrgs.toRemove);
-    }
-
-    let membersToRemove: string[] = [];
-    if (original.members != null) {
-      membersToRemove = getDataItemDuplicates(original.members, toPatch.members.toRemove);
-    }
-
-    /**
-     * Check to find only additions that does not exist in the original. If they already
-     * exist in the original, they do not need to be included in the request for addition.
-     */
-    let subOrgsToAdd: string[] = [];
-    if (original.subordinateOrganizations != null) {
-      subOrgsToAdd = getDataItemNonDuplicates(original.subordinateOrganizations, toPatch.subOrgs.toAdd);
-    } else {
-      subOrgsToAdd = mapDataItemsToStringIds(toPatch.subOrgs.toAdd);
-    }
-
-    let membersToAdd: string[] = [];
-    if (original.members != null) {
-      membersToAdd = getDataItemNonDuplicates(original.members, toPatch.members.toAdd);
-    } else {
-      membersToAdd = mapDataItemsToStringIds(toPatch.members.toAdd);
-    }
-
     const requests: Promise<any>[] = [];
-
     /**
      * Keep track of the indexes for the requests that are made
      * to help differentiate the messages on failure of any request.
-     * Ensures that only the requests that must be made are actually sent.
      */
     const requestMap: Map<number, OrganizationPatchRequestType> = new Map();
-    if (jsonPatchOperations.length > 0) {
-      requests.push(this.orgApi.jsonPatchOrganization(toUpdate.id, jsonPatchOperations));
-      requestMap.set(requests.length - 1, OrganizationPatchRequestType.JSON_PATCH);
-    }
 
-    if (subOrgsToRemove.length > 0) {
-      requests.push(this.orgApi.removeSubordinateOrganization(toUpdate.id, subOrgsToRemove));
-      requestMap.set(requests.length - 1, OrganizationPatchRequestType.SUB_ORG_REMOVE);
-    }
-
-    if (subOrgsToAdd.length > 0) {
-      requests.push(this.orgApi.addSubordinateOrganization(toUpdate.id, subOrgsToAdd));
-      requestMap.set(requests.length - 1, OrganizationPatchRequestType.SUB_ORG_ADD);
-    }
-
-    if (membersToRemove.length > 0) {
-      requests.push(this.orgApi.deleteOrganizationMember(toUpdate.id, membersToRemove));
-      requestMap.set(requests.length - 1, OrganizationPatchRequestType.MEMBERS_REMOVE);
-    }
-
-    if (membersToAdd.length > 0) {
-      requests.push(this.orgApi.addOrganizationMember(toUpdate.id, membersToAdd));
-      requestMap.set(requests.length - 1, OrganizationPatchRequestType.MEMBERS_ADD);
-    }
+    this.createJsonPatchRequests(original, toUpdate, toPatch, requests, requestMap);
 
     const data = await Promise.allSettled(requests);
     const failures = data.some(request => request.status === 'rejected');
@@ -553,6 +499,84 @@ export default class OrganizationService extends AbstractDataService<Organizatio
       type: ResponseType.SUCCESS,
       data: this.convertOrgDetailsToDto(toUpdate)
     });
+  }
+
+  /**
+   * 
+   * Generates all of the necessary requests that need to be made to PATCH an Organization.
+   * Only the requests that are necessary to update an organization will be performed.
+   * 
+   * @param originalOrg the original organization being updated
+   * @param updatedOrg the updated organization
+   * @param patchState the extended state to generate patch operations
+   * @param listOfRequests the list of requests that will be made
+   * @param mapOfRequests the map that maps the index of a request in listOfRequests to its patch request type
+   * 
+   * @throws throws error if the updated organzation does not contain an ID
+   */
+  createJsonPatchRequests(originalOrg: OrganizationDtoWithDetails, updatedOrg: OrganizationDtoWithDetails, patchState: OrganizationEditState, listOfRequests: Promise<any>[], mapOfRequests: Map<number, OrganizationPatchRequestType>): void {
+    const jsonPatchOperations: Array<JsonPatchStringArrayValue | JsonPatchStringValue | JsonPatchObjectValue | JsonPatchObjectArrayValue> = this.createJsonPatch(originalOrg, updatedOrg, patchState);
+
+    if (updatedOrg.id == null) {
+      throw new Error('Organization to update has undefined id.');
+    }
+
+    /**
+     * Check to find only removals that exist in the original. If it does not exist
+     * in the original, then there's no need to add it to the request for removal.
+     */
+    let subOrgsToRemove: string[] = [];
+    if (originalOrg.subordinateOrganizations != null) {
+      subOrgsToRemove = getDataItemDuplicates(originalOrg.subordinateOrganizations, patchState.subOrgs.toRemove);
+    }
+
+    let membersToRemove: string[] = [];
+    if (originalOrg.members != null) {
+      membersToRemove = getDataItemDuplicates(originalOrg.members, patchState.members.toRemove);
+    }
+
+    /**
+     * Check to find only additions that does not exist in the original. If they already
+     * exist in the original, they do not need to be included in the request for addition.
+     */
+    let subOrgsToAdd: string[] = [];
+    if (originalOrg.subordinateOrganizations != null) {
+      subOrgsToAdd = getDataItemNonDuplicates(originalOrg.subordinateOrganizations, patchState.subOrgs.toAdd);
+    } else {
+      subOrgsToAdd = mapDataItemsToStringIds(patchState.subOrgs.toAdd);
+    }
+
+    let membersToAdd: string[] = [];
+    if (originalOrg.members != null) {
+      membersToAdd = getDataItemNonDuplicates(originalOrg.members, patchState.members.toAdd);
+    } else {
+      membersToAdd = mapDataItemsToStringIds(patchState.members.toAdd);
+    }
+
+    if (jsonPatchOperations.length > 0) {
+      listOfRequests.push(this.orgApi.jsonPatchOrganization(updatedOrg.id, jsonPatchOperations));
+      mapOfRequests.set(listOfRequests.length - 1, OrganizationPatchRequestType.JSON_PATCH);
+    }
+
+    if (subOrgsToRemove.length > 0) {
+      listOfRequests.push(this.orgApi.removeSubordinateOrganization(updatedOrg.id, subOrgsToRemove));
+      mapOfRequests.set(listOfRequests.length - 1, OrganizationPatchRequestType.SUB_ORG_REMOVE);
+    }
+
+    if (subOrgsToAdd.length > 0) {
+      listOfRequests.push(this.orgApi.addSubordinateOrganization(updatedOrg.id, subOrgsToAdd));
+      mapOfRequests.set(listOfRequests.length - 1, OrganizationPatchRequestType.SUB_ORG_ADD);
+    }
+
+    if (membersToRemove.length > 0) {
+      listOfRequests.push(this.orgApi.deleteOrganizationMember(updatedOrg.id, membersToRemove));
+      mapOfRequests.set(listOfRequests.length - 1, OrganizationPatchRequestType.MEMBERS_REMOVE);
+    }
+
+    if (membersToAdd.length > 0) {
+      listOfRequests.push(this.orgApi.addOrganizationMember(updatedOrg.id, membersToAdd));
+      mapOfRequests.set(listOfRequests.length - 1, OrganizationPatchRequestType.MEMBERS_ADD);
+    }
   }
 
   /**
