6b996c7cdc1b0326549d23a62350ae2b81e9aa89
Dylan Nakamoto <dnakamoto.ctr@revacomm.com>
2021-07-19 21:20:28 -1000

    Remove unused styles

--- src/components/forms/TextInputWithDelete/TextInputWithDelete.tsx
+++ src/components/forms/TextInputWithDelete/TextInputWithDelete.tsx
@@ -2,9 +2,6 @@ import TextInput from '../TextInput/TextInput';
 import { TextInputProps } from '../TextInput/TextInputProps';
 import { TextInputWithDeleteProps } from './TextInputWithDeleteProps';
 
-import './TextInputWithDelete.scss';
-
-
 function TextInputWithDelete({ ...props }: TextInputProps & TextInputWithDeleteProps) {
   return (
     <TextInput withDelete {...props} />
