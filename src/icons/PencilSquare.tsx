bd85b32c8450b213724b3a8e816f6db0df03ebf9
Donn Eddy <maindric2@gmail.com>
2021-08-18 08:59:55 +0930

    Updated margin on icon.

--- src/icons/PencilSquare.tsx
+++ src/icons/PencilSquare.tsx
@@ -4,7 +4,7 @@ import {IconProps} from './IconProps';
 function PencilSquare(props: IconProps) {
   return (
       <i className={`pencil-square bi bi-pencil-square pencil-square-color ${props.className}`}
-         style={{fontSize: `${props.size}g`, margin: `0 4px 0 0`}}
+         style={{fontSize: `${props.size}g`, margin: `0 0.75rem 0 0`}}
          title={ props.iconTitle != null ? props.iconTitle : 'pencil-square'}
       ></i>
   );
