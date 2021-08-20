63c80ac92fe089c570cfdf80b3a42a6264e44882
Chad Kumabe <ckumabe@gmail.com>
2021-08-20 12:01:08 -1000

    [TRONAPI-603] Updated header info styles.

---
+++ src/components/InfoNotice/InfoNotice.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './InfoNotice.scss';
+import {InfoNoticeProps} from './InfoNoticeProps';
+
+function InfoNotice(props: InfoNoticeProps) {
+  return (
+      <div className="info-notice">
+        <
+      </div>
+  );
+}
+
+export default InfoNotice;
