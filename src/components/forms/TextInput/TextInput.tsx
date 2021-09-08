12a2dd580d74cc81f9ec1a96f1f3c7a173a0932f
Chad Kumabe <ckumabe@gmail.com>
2021-09-07 16:31:36 -1000

    [TRONAPI-427] Updated styles for input search icon.

--- src/components/forms/TextInput/TextInput.tsx
+++ src/components/forms/TextInput/TextInput.tsx
@@ -6,23 +6,25 @@ import { TextInputWithDeleteProps } from '../TextInputWithDelete/TextInputWithDe
 import Button from '../../Button/Button';
 import CloseIcon from '../../../icons/CloseIcon';
 import SearchIcon from '../../../icons/SearchIcon';
+import {useMemo} from 'react';
 
 function TextInput({ appendedText, deleteButtonTitle, onDeleteClickHandler, withDelete, searchInput, ...props }: TextInputProps & Partial<TextInputWithDeleteProps>) {
   const hasValidValue = props.value != null && String(props.value).trim().length > 0;
+  const inputClass: string | undefined = useMemo(() => {
+    return props.className != null ? `${props.className} tron-text-input__search-bar` : undefined;
+  }, [props.className]);
   return (
     <div className={`tron-text-input${appendedText ? ' tron-text-input--appended' : ''}${props.className ? (' ' + props.className) : ''}`}>
       <div className={`tron-text-input__input-container${withDelete ? ' tron-text-input__input-container--delete' : ''}`}>
-        {searchInput && 
+        {searchInput &&
           <SearchIcon
-            iconTitle="Search" 
-            size={1.5} 
+            iconTitle="Search"
+            size={1.25}
           />
         }
-        <UswdsTextInput 
-          {...props} 
-          // I tried to do classes for the following, but there is something wrong with className
-          // on this object.  Class would be 'tron-text-input--search-bar'.
-          style={searchInput ? {padding:'0 0 0 2.2rem'} : {}} 
+        <UswdsTextInput
+          {...props}
+            className={inputClass}
         />
         {withDelete && hasValidValue &&
           <Button
--- src/icons/SearchIcon.tsx
+++ src/icons/SearchIcon.tsx
@@ -3,7 +3,7 @@ import { IconProps } from './IconProps';
 function SearchIcon(props: IconProps) {
   return (
       <i className={`search-icon bi bi-search ${props.className}`}
-         style={{fontSize: `${props.size}rem`, position:'absolute', margin:`.35rem`}}
+         style={{fontSize: `${props.size}rem`}}
          title={ props.iconTitle != null ? props.iconTitle : 'search'}
       ></i>
   );
