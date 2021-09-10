055db4f23126b87b416959bb311dc8cb7f5b88d9
Christopher Zell <zellcc@revacomm.com>
2021-09-10 12:46:07 -0400

    adds beautify json button for key value editor

--- src/pages/ScratchStorage/ScratchStorageKeyValueEditorForm.tsx
+++ src/pages/ScratchStorage/ScratchStorageKeyValueEditorForm.tsx
@@ -1,17 +1,18 @@
-import { State } from "@hookstate/core";
-import { Initial } from '@hookstate/initial';
-import { Validation } from '@hookstate/validation';
-import React, { FormEvent, useEffect } from 'react';
-import Button from '../../components/Button/Button';
-import Form from "../../components/forms/Form/Form";
-import FormGroup from '../../components/forms/FormGroup/FormGroup';
+import { State, useHookstate } from "@hookstate/core";
+import { Initial } from "@hookstate/initial";
+import React, { useEffect } from "react";
+import Button from "../../components/Button/Button";
+import FormGroup from "../../components/forms/FormGroup/FormGroup";
 import TextInput from "../../components/forms/TextInput/TextInput";
-import { ScratchStorageEntryDto } from '../../openapi';
-import { useScratchStorageState } from '../../state/scratch-storage/scratch-storage-state';
-import { failsHookstateValidation, generateStringErrorMessages } from '../../utils/validation-utils';
-import { ScratchStorageCreateUpdateState } from './ScratchStorageEditForm';
-import './ScratchStorageKeyValueEditorForm.scss';
-import './ScratchStorageUserAddForm.scss';
+import { ScratchStorageEntryDto } from "../../openapi";
+import { useScratchStorageState } from "../../state/scratch-storage/scratch-storage-state";
+import {
+  failsHookstateValidation,
+  generateStringErrorMessages
+} from "../../utils/validation-utils";
+import { ScratchStorageCreateUpdateState } from "./ScratchStorageEditForm";
+import "./ScratchStorageKeyValueEditorForm.scss";
+import "./ScratchStorageUserAddForm.scss";
 
 interface ScratchStorageAddKVPProps {
   createUpdateState: State<ScratchStorageCreateUpdateState>;
@@ -19,33 +20,68 @@ interface ScratchStorageAddKVPProps {
   valueState: State<string>;
 }
 
+export interface JsonValueError {
+  error: boolean;
+  message: string;
+}
+
 function ScratchStorageKeyValueEditorForm(props: ScratchStorageAddKVPProps) {
   const scratchStorageService = useScratchStorageState(); // handle to the scratch storage service
-  const [localValue, setLocalValue] = React.useState('');
+  
+  // the value of the key (raw string)
+  const localValueState = useHookstate<string>("");
+
+  // indicate any json parse errors
+  const jsonErrorState = useHookstate<JsonValueError>({
+    error: false,
+    message: "",
+  });
 
   // if we're editing an existing key value then fetch it's value from the API,
   // if its not already in the service createUpdate state... this would be the case if the key has
   // been created in the editor, but never committed to the backend (like the user recalled it and wanted to edit it one more time before committing)
   useEffect(() => {
-    const kvpIndex = scratchStorageService.createUpdateState.findIndex(item => item.key.get() === props.createUpdateState.keyName.get());
+    const kvpIndex = scratchStorageService.createUpdateState.findIndex(
+      (item) => item.key.get() === props.createUpdateState.keyName.get()
+    );
     if (props.createUpdateState.isEdit.value && kvpIndex === -1) {
-        scratchStorageService
-            .scratchStorageApi
-            .getKeyValueByKeyName(props.createUpdateState.appId.value, props.createUpdateState.keyName.value)
-                .then(resp => {
-                  setLocalValue(resp.data.value ?? '');
-                  props.valueState.set(resp.data.value ?? '');
-                });
-    }
-    else if (props.createUpdateState.isEdit.value) {
-      setLocalValue(scratchStorageService.createUpdateState[kvpIndex].value.value ?? '');
-      props.valueState.set(scratchStorageService.createUpdateState[kvpIndex].value.value ?? '');
+      scratchStorageService.scratchStorageApi
+        .getKeyValueByKeyName(
+          props.createUpdateState.appId.value,
+          props.createUpdateState.keyName.value
+        )
+        .then((resp) => {
+          localValueState.set(resp.data.value ?? "");
+          props.valueState.set(resp.data.value ?? "");
+        });
+    } else if (props.createUpdateState.isEdit.value) {
+      localValueState.set(
+        scratchStorageService.createUpdateState[kvpIndex].value.value ?? ""
+      );
+      props.valueState.set(
+        scratchStorageService.createUpdateState[kvpIndex].value.value ?? ""
+      );
     }
   }, []);
 
   const isKeyNameModified = (): boolean => {
-    return Initial(props.createUpdateState.keyName).modified() ;
-  }
+    return Initial(props.createUpdateState.keyName).modified();
+  };
+
+  // attempts to beautify a string into pretty JSON
+  const beautifyJsonValue = (): void => {
+    try {
+      const newValue = JSON.stringify(
+        JSON.parse(localValueState.get()),
+        undefined,
+        2
+      );
+      jsonErrorState.set({ error: false, message: "" });
+      localValueState.set(newValue);
+    } catch (e) {
+      jsonErrorState.set({ error: true, message: "Could not beautify JSON" });
+    }
+  };
 
   return (
     <div
@@ -53,28 +89,50 @@ function ScratchStorageKeyValueEditorForm(props: ScratchStorageAddKVPProps) {
       data-testid="scratch-storage-add-kvp-form"
     >
       <FormGroup
-          labelName="keyName"
-          labelText="Key Name"
-          isError={isKeyNameModified() && failsHookstateValidation(props.createUpdateState.keyName)}
-          errorMessages={generateStringErrorMessages(props.createUpdateState.keyName)}
-          required={!props.createUpdateState.isEdit.value}
+        labelName="keyName"
+        labelText="Key Name"
+        isError={
+          isKeyNameModified() &&
+          failsHookstateValidation(props.createUpdateState.keyName)
+        }
+        errorMessages={generateStringErrorMessages(
+          props.createUpdateState.keyName
+        )}
+        required={!props.createUpdateState.isEdit.value}
       >
         <TextInput
-            id="keyName"
-            name="keyName"
-            type="text"
-            autoFocus={true}
-            data-testid='scratch-kvp-keyname'
-            disabled={props.createUpdateState.isEdit.value}
-            value={props.createUpdateState.keyName.get()}
-            error={isKeyNameModified() && failsHookstateValidation(props.createUpdateState.keyName)}
-            onChange={(event) => props.createUpdateState.keyName.set(event.target.value)}
+          id="keyName"
+          name="keyName"
+          type="text"
+          autoFocus={true}
+          data-testid="scratch-kvp-keyname"
+          disabled={props.createUpdateState.isEdit.value}
+          value={props.createUpdateState.keyName.get()}
+          error={
+            isKeyNameModified() &&
+            failsHookstateValidation(props.createUpdateState.keyName)
+          }
+          onChange={(event) =>
+            props.createUpdateState.keyName.set(event.target.value)
+          }
         />
       </FormGroup>
-
       <FormGroup
-          labelName="keyValue"
-          labelText="Key Value"
+        labelName="keyValue"
+        labelText="Key Value"
+        isError={jsonErrorState.get().error}
+        errorMessages={[jsonErrorState.message.value]}
+        actionsNode={
+          <div className="action-area">
+            <Button
+              type="button"
+              className="button"
+              onClick={beautifyJsonValue}
+            >
+              Beautify Json
+            </Button>
+          </div>
+        }
       >
         <textarea
           className="value-editor"
@@ -82,17 +140,15 @@ function ScratchStorageKeyValueEditorForm(props: ScratchStorageAddKVPProps) {
           name="keyValue"
           data-testid="scratch-kvp-value"
           rows={20}
-          onChange={(event) =>  {
+          onChange={(event) => {
             props.valueState.set(event.target.value);
-            setLocalValue(event.target.value);
+            localValueState.set(event.target.value);
           }}
-          value={localValue}
-      />
-        <br/>
-      </FormGroup>
+          value={localValueState.get()}
+        />
+      </FormGroup>      
     </div>
   );
-
 }
 
 export default ScratchStorageKeyValueEditorForm;
