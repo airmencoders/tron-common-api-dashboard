07a61cce016cb7d381aa411095481d89c3b655f1
Christopher Zell <zellcc@revacomm.com>
2021-09-30 15:00:24 -0400

    adds the tooltip for version

--- src/components/AppInfoTag/AppInfoTag.tsx
+++ src/components/AppInfoTag/AppInfoTag.tsx
@@ -6,6 +6,7 @@ import './AppInfoTag.scss';
 
 export default function AppInfoTag() {
   const appInfoService = useAppVersionState();
+  const [tipClass, setTipClass] = React.useState('sidebar__tags-tip-hidden');
   const [enclave, setEnclave] = React.useState<string>('');
   const [env, setEnv] = React.useState<string>('');
 
@@ -14,33 +15,49 @@ export default function AppInfoTag() {
   }, []);
 
   function getClassName() {
+    if (appInfoService.state.enclave.get()?.toUpperCase() === 'IL4') {
+      setEnclave('il4');
+    } else if (appInfoService.state.enclave.get()?.toUpperCase() === 'IL2') {
+      setEnclave('il2');
+    } else {
+      setEnclave('???');
+    }
 
-      if (appInfoService.state.enclave.get()?.toUpperCase() === 'IL4') {
-        setEnclave('il4');
-      } else if (appInfoService.state.enclave.get()?.toUpperCase() === 'IL2') {
-        setEnclave('il2');
-      } else {
-        setEnclave('???');
-      }
-
-      if (
-        appInfoService.state.environment.get()?.toUpperCase() === 'PRODUCTION'
-      ) {
-        setEnv('prod');
-      } else if (
-        appInfoService.state.environment.get()?.toUpperCase() === 'STAGING'
-      ) {
-        setEnv('staging');
-      } else {
-        setEnv('---')
-      }
+    if (
+      appInfoService.state.environment.get()?.toUpperCase() === 'PRODUCTION'
+    ) {
+      setEnv('prod');
+    } else if (
+      appInfoService.state.environment.get()?.toUpperCase() === 'STAGING'
+    ) {
+      setEnv('staging');
+    } else {
+      setEnv('---');
+    }
   }
 
   return (
-    <div className="sidebar__tags-container">
-      <Tag className="sidebar__apptag">{
-        <><span className={`enclave__${enclave}`}>{`${appInfoService.state.enclave.get()?.toUpperCase()} `}</span><span className={`environ__${env}`}>{` ${appInfoService.state.environment.get()?.toUpperCase()} `}</span></>
-      }</Tag>
+    <div className="sidebar__tags-container"
+    onMouseOver={() => setTipClass('sidebar__tags-tip-show')}
+    onMouseLeave={() => setTipClass('sidebar__tags-tip-hidden')}>
+      <Tag className="sidebar__apptag">
+        {
+          <>
+            <span className={`enclave__${enclave}`}>{`${
+              appInfoService.state.enclave.get()?.toUpperCase() ?? ''
+            } `}</span>
+            <span className={`environ__${env}`}>{` ${
+              appInfoService.state.environment.get()?.toUpperCase() ?? ''
+            } `}</span>
+                  <div
+        className={tipClass}
+      >
+        {`App Ver: ${appInfoService.state.version.get() ?? 'Unknown'}`}
+      </div>
+          </>
+        }
+      </Tag>
+
     </div>
   );
 }
