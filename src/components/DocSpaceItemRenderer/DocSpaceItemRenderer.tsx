eb1ba19d1d5a603a808d2b269bf9dd5f670847b8
Christopher Zell <zellcc@revacomm.com>
2021-11-22 09:36:35 -0500

    making sonarqube happy

--- src/components/DocSpaceItemRenderer/DocSpaceItemRenderer.tsx
+++ src/components/DocSpaceItemRenderer/DocSpaceItemRenderer.tsx
@@ -4,9 +4,9 @@ import { DocumentDto } from '../../openapi';
 import Button from '../Button/Button';
 import Spinner from '../Spinner/Spinner';
 import './DocSpaceItemRenderer.scss';
-import {ICellRendererParams} from 'ag-grid-community';
-import {ClickableCellRenderer} from '../Grid/clickable-cell-renderer';
-import {useDocumentSpaceState} from '../../state/document-space/document-space-state';
+import { ICellRendererParams } from 'ag-grid-community';
+import { ClickableCellRenderer } from '../Grid/clickable-cell-renderer';
+import { useDocumentSpaceState } from '../../state/document-space/document-space-state';
 
 export interface DocSpaceItemRendererProps {
   hideItemLink?: boolean;
@@ -17,7 +17,6 @@ export interface DocSpaceItemRendererProps {
  * directories and files
  */
 function DocSpaceItemRenderer(props: Partial<ICellRendererParams> & ClickableCellRenderer & DocSpaceItemRendererProps) {
-
   const documentSpaceService = useDocumentSpaceState();
 
   const data = props.node?.data as DocumentDto;
@@ -29,39 +28,48 @@ function DocSpaceItemRenderer(props: Partial<ICellRendererParams> & ClickableCel
     return <Spinner small />;
   }
 
+  function renderFolderItem() {
+    if (props.hideItemLink) {
+      return <span>{data.key}</span>;
+    } else {
+      return (
+        <Button
+          type="button"
+          unstyled
+          data-testid="docspace-row-item"
+          onClick={() => {
+            if (data.folder) {
+              props.onClick(data.key);
+            }
+          }}
+        >
+          <span className="directory">{data.key}</span>
+        </Button>
+      );
+    }
+  }
+
+  function renderFileItem() {
+    if (props.hideItemLink) {
+      return <span>{data.key}</span>;
+    } else {
+      return (
+        <a
+          href={documentSpaceService.createRelativeDownloadFileUrl(space, path, fileKey)}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {data.key}
+        </a>
+      );
+    }
+  }
+
   return (
-    <div
-      className="loading-cell-renderer"
-      data-testid={`docspace-item-cell-renderer__${data.key}`}
-    >
+    <div className="loading-cell-renderer" data-testid={`docspace-item-cell-renderer__${data.key}`}>
       {data.folder ? <FolderIcon /> : null}
       {'  '}
-      {data.folder ?
-        props.hideItemLink ?
-          <span>{data.key}</span>
-        :
-          <Button
-            type="button"
-            unstyled
-            data-testid="docspace-row-item"
-            onClick={() => {
-              if (data.folder) {
-                props.onClick(data.key);
-              }
-            }}
-          >
-            <span className='directory'>{data.key}</span>
-          </Button>
-      :
-        props.hideItemLink ?
-          <span>{data.key}</span>        
-        :
-          <a href={documentSpaceService.createRelativeDownloadFileUrl(
-            space,
-            path,
-            fileKey
-          )} target="_blank" rel="noreferrer">{data.key}</a>
-      }
+      {data.folder ? renderFolderItem() : renderFileItem()}
     </div>
   );
 }
